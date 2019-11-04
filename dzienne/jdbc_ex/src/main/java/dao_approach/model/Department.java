88f24400b24cf9bd0ccbe5c6ce5528680f529785
kpodlaski <podlaski@uni.lodz.pl>
2019-11-04 10:57:58 +0100

    model created

--- dzienne/jdbc_ex/src/main/java/dao_approach/model/Department.java
+++ dzienne/jdbc_ex/src/main/java/dao_approach/model/Department.java
@@ -1,4 +1,46 @@
 package dao_approach.model;
 
+import java.util.List;
+
 public class Department {
+    private int id;
+    private String Name;
+    private List<Person> members;
+
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public List<Person> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Person> members) {
+        this.members = members;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(this.id);
+    }
 }
--- dzienne/jdbc_ex/src/main/java/dao_approach/model/Person.java
+++ dzienne/jdbc_ex/src/main/java/dao_approach/model/Person.java
@@ -1,4 +1,64 @@
 package dao_approach.model;
 
+import java.util.Objects;
+
 public class Person {
+    private int id;
+    private String name;
+    private  String surname;
+    private String position;
+    private int departmentId;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
 }
